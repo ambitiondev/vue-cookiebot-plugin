8340c3010fefd2d02bb0dc6e20c71355f86e4541
Berrie Nachtweh <bnachtweh@netvlies.net>
2020-09-01 09:12:09 +0200

    rm console log

--- src/plugin.ts
+++ src/plugin.ts
@@ -4,6 +4,4 @@ import { CookieBot } from './cookiebot'
 
 export const install: PluginFunction<CookieBotConfig> = (Vue: typeof VueImport, config?: CookieBotConfig): void => {
     Vue.prototype.$cookiebot = new CookieBot(config)
-
-    return console.log('installed cookiebot')
 }
