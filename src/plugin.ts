324d2d713b12d2b773fd14b56af7d0b969c0aa0a
Naw <naw_123@hotmail.nl>
2021-12-03 16:35:10 +0100

    Fixed Small Bug

--- src/plugin.ts
+++ src/plugin.ts
@@ -3,11 +3,11 @@ import { CookieBotConfig } from '../types/cookiebot'
 import { CookieBot } from './cookiebot'
 
 export const install: PluginFunction<CookieBotConfig> = (Vue: typeof VueImport, config?: CookieBotConfig): void => {
-    if (config) {
-        if (Vue.version >= 3.0) {
-            Vue.provide('$cookiebot', new CookieBot(config)
-        } else {
-            Vue.prototype.$cookiebot = new CookieBot(config)
-        }                        
-    }
-}
+    if (!config) return
+    if (Vue.version >= "3.0") {
+        // @ts-ignore
+        Vue.provide('$cookiebot', new CookieBot(config))
+      } else {
+         Vue.prototype.$cookiebot = new CookieBot(config)
+      }
+};
